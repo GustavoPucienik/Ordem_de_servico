337422bbe344c1f02ee12f71874f5c92f4014344
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-11-11 19:16:47 -0300

    Criando rota editarOrdem

---
+++ ordem-de-servico/src/components/RotaEditarOrdem/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header'
+import BodyEditarOrdem from "../BodyEditarOrdem"
+
+const RotaEditarOrdem = () => {
+  return (
+    <div>
+      <Header nome="ordem-req"/>
+      <BodyEditarOrdem />
+    </div>
+  )
+}
+
+export default RotaEditarOrdem
