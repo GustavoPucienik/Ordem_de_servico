c970e2c50c87ee128aed843e761d8e4a3d5cf7da
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-11-11 18:41:22 -0300

    Commit teste

--- ordem-de-servico/src/App.js
+++ ordem-de-servico/src/App.js
@@ -11,7 +11,6 @@ import OrdemRequisitada from './components/OrdemRequisitada'
 import RotaOrdensConcluidas from './components/RotaOrdensConcluidas'
 
 function App() {
- const dados;
   return (
     <div className="App">
       <BrowserRouter>
