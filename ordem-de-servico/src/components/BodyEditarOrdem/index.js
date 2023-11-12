5ae4b16b5c90ec4217e2df810f5138ea56f70661
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-11-12 14:37:09 -0300

    Carregando dados pré definidos na rota editar ordem/:id

--- ordem-de-servico/src/components/BodyEditarOrdem/index.js
+++ ordem-de-servico/src/components/BodyEditarOrdem/index.js
@@ -10,35 +10,28 @@ const URLPegaRequisicao = `${API_BASE_URL}/ordens/`;
 const BodyOrdemReq = () => {
   const { id } = useParams();
   const [dados, setDados] = useState([]);
-  const [formData, setFormData] = useState({
-    tipo_servico: "",
-    inicio: "",
-    termino: "",
-    tempo: "",
-    parada_maquina: "",
-    mecanicos: "",
-    item_defeito: "",
-    problema: "",
-    solucao: "",
-    concluida: ""
-  });  
 
   useEffect(()=> {
     verificacaoUsuarioManutencao();
     axios.get(`${URLPegaRequisicao}${id}`, {
     })
     .then( function (response){
+      // Formatar datas usando o objeto Date
+      const formattedInicio = response.data.inicio ? new Date(response.data.inicio).toISOString().slice(0, 16) : '';
+      const formattedTermino = response.data.termino ? new Date(response.data.termino).toISOString().slice(0, 16) : '';
+      response.data.inicio = formattedInicio;
+      response.data.termino = formattedTermino;
       setDados(response.data);
-      //setFormData({ ...formData, mecanicoQresolveu: dados.nome })
+
     })
-  },[]);
+  },[id]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
   
     try {
       // Envie os dados do formulário para o backend para atualizar a ordem
-      await axios.put(`${URLPegaRequisicao}${id}`, formData);
+      await axios.put(`${URLPegaRequisicao}${id}`, dados);
       alert(`Ordem de serviço atualizada com sucesso!`);
       
     } catch (error) {
@@ -64,8 +57,8 @@ const BodyOrdemReq = () => {
               <div className={styles.tipoServico}>
                 <h1 className={styles.titulos}>Relátorio técnico</h1>
                 <p className={styles.pTempo}>Que tipo de manutenção foi realizada:</p>
-                <select name="tipo_servico" 
-                  onChange={(e) => setFormData({ ...formData, tipo_servico: e.target.value })}>
+                <select name="tipo_servico" id='servicoSelecionado' value={dados.tipo_servico}
+                  onChange={(e) => setDados({ ...dados, tipo_servico: e.target.value })}>
                   <option value="">Escolha aqui</option>
                   <option value="C. Emergencial">C. Emergencial</option>
                   <option value="Predial">Predial</option>
@@ -75,36 +68,36 @@ const BodyOrdemReq = () => {
               <div className={styles.tempo}>
                 <p className={styles.pTempo}>Inicio da manutenção:</p>
                 <input required className={styles.inputData} type="datetime-local" name="inicio"
-                  value={formData.inicio} onChange={(e) => setFormData({ ...formData, inicio: e.target.value })} />
+                  value={dados.inicio} onChange={(e) => setDados({ ...dados, inicio: e.target.value })} />
                 <p className={styles.pTempo}>Fim da manutenção:</p>
                 <input required className={styles.inputData} type="datetime-local" name="termino" 
-                  value={formData.termino} onChange={(e) => setFormData({ ...formData, termino: e.target.value })}/>
+                  value={dados.termino} onChange={(e) => setDados({ ...dados, termino: e.target.value })}/>
                 <p className={styles.pTempo}>Tempo da manutenção:</p>
                 <input required className={styles.inputData} type="time" name="tempo" 
-                  value={formData.tempo} onChange={(e) => setFormData({ ...formData, tempo: e.target.value })} />
+                  value={dados.tempo} onChange={(e) => setDados({ ...dados, tempo: e.target.value })} />
                 <p className={styles.pTempo}>Tempo que a linha parou:</p>
                 <input className={styles.inputData} type="time" name="parada_maquina"
-                 value={formData.parada_maquina} onChange={(e) => setFormData({ ...formData, parada_maquina: e.target.value })} />
+                 value={dados.parada_maquina} onChange={(e) => setDados({ ...dados, parada_maquina: e.target.value })} />
               </div>
               <div className={styles.relatorio}>
                 <p>Coloque abaixo o nome dos tecnicos</p>
-                <input required type="text" placeholder='Nome de quem realizou a manutenção' name='mecanicos' value={formData.mecanicos} 
-                  onChange={(e) => setFormData({ ...formData, mecanicos: e.target.value })}/><br />
+                <input required type="text" placeholder='Nome de quem realizou a manutenção' name='mecanicos' value={dados.mecanicos} 
+                  onChange={(e) => setDados({ ...dados, mecanicos: e.target.value })}/><br />
                 <p>Item com defeito:</p>
-                <input required type="text" placeholder='Peças, maquinas etc...' name='item_defeito' value={formData.item_defeito}
-                    onChange={(e) => setFormData({ ...formData, item_defeito: e.target.value })} /><br />
+                <input required type="text" placeholder='Peças, maquinas etc...' name='item_defeito' value={dados.item_defeito}
+                    onChange={(e) => setDados({ ...dados, item_defeito: e.target.value })} /><br />
                 <p>Defeito do item:</p>
-                <input required type="text" placeholder='Falha apresentada' name='problema' value={formData.problema} 
-                  onChange={(e) => setFormData({ ...formData, problema: e.target.value })} /><br />
+                <input required type="text" placeholder='Defeito do item' name='problema' value={dados ? dados.problema : ""} 
+                  onChange={(e) => setDados({ ...dados, problema: e.target.value })} /><br />
 
                 <p>Descreva o serviço realizado</p>
                 <textarea required type="text" placeholder="Descreva aqui o serviço realizado"
                   name='solucao' 
                   maxLength={255}
-                  value={formData.solucao}
-                  onChange={(e) => setFormData({ ...formData, solucao: e.target.value, concluida:true })} /><br />
+                  value={dados.solucao}
+                  onChange={(e) => setDados({ ...dados, solucao: e.target.value, concluida:true })} /><br />
 
-                <button type="submit">Fechar Ordem</button>
+                <button type="submit">Editar Ordem</button>
               </div>
             </form>
         </div>
