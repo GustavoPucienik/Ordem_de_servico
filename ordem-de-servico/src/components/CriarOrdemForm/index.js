4506b4b2eac03323b7c8aad1659d548111e8edd6
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-11-13 01:08:38 -0300

    Criando requisição das linhas da tabela linhas, e substituido as linhas estaticas pelas do banco de dados

--- ordem-de-servico/src/components/CriarOrdemForm/index.js
+++ ordem-de-servico/src/components/CriarOrdemForm/index.js
@@ -5,9 +5,11 @@ import { API_BASE_URL } from '../../config';
 
 const URLPegaDados = `${API_BASE_URL}/dadosusuario`;
 const URLCadastraOrdem = `${API_BASE_URL}/criarOrdem`;
+const URLPegaLinhas = `${API_BASE_URL}/linha`
 
 const CriarOrdemForm = () => {
   const [dados, setDados] = useState(null);
+  const [linhas, setLinhas] = useState([]);
   const [formData, setFormData] = useState({
     usuario_req: null,
     setor: null,
@@ -33,6 +35,12 @@ const CriarOrdemForm = () => {
       } catch (error) {
         alert( error);
         window.location = "/login";
+      }
+      try {
+        const linhasRes = await axios.get(URLPegaLinhas)
+        setLinhas(linhasRes.data);
+      } catch (error) {
+        alert( error);
       }
     };
     fetchData();
@@ -68,30 +76,9 @@ const CriarOrdemForm = () => {
       <input className={styles.inputCriarOrdem} name="setor" readOnly value={formData.setor}
       onChange={handleChange}/><br/>
         <select name="linha" className={styles.selectCriaOrdem} onChange={handleChange}>
-          <option selected value="">Selecione a linha</option>
-          <option value="Blistagem">Blistagem</option>
-          <option value="Arla">Arla</option>
-          <option value="Nannini">Nannini</option>
-          <option value="Tecnoenvase">Tecnoenvase</option>
-          <option value="Serac">Serac</option>
-          <option value="Bosch">Bosch</option>
-          <option value="Multiuso">Multiuso</option>
-          <option value="Horse">Horse</option>
-          <option value="Kalix">Kalix</option>
-          <option value="LPB">LPB</option>
-          <option value="Terco 1">Terco 1</option>
-          <option value="Terco 2">Terco 2</option>
-          <option value="Una 1">Una 1</option>
-          <option value="Una 2">Una 2</option>
-          <option value="Promaquina">Promaquina</option>
-          <option value="Sache">Sache</option>
-          <option value="Cósméticos">Cósméticos</option>
-          <option value="Manual">Manual</option>
-          <option value="Manual 200L / 100L">Manual 200L / 100L</option>
-          <option value="Montagem de kit">Montagem de kit</option>
-          <option value="Dosagem grafite">Dosagem grafite</option>
-          <option value="Dosagem selante">Dosagem selante</option>
-          <option value="Dosagem graxa">Dosagem graxa</option>
+          {linhas? linhas.map((req, index) => (
+            <option value={req.nomeDaLinha}>{req.nomeDaLinha}</option>
+          )) : ""}
         </select><br />
         <textarea className={styles.descricaoReq} type="text" name="descricao_req" onChange={handleChange}
         placeholder="Descreva o problema aqui..." maxLength={255} />
