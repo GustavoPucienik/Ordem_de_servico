3ca1b58d883ec2626091ce0e4b10b1f862a556f5
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-11-13 00:38:55 -0300

    Criando método de atualizar linha e pegar todas as linhas

--- Back-end/api/controllers/linhasController.js
+++ Back-end/api/controllers/linhasController.js
@@ -16,6 +16,33 @@ class LinhasController {
     }
   }
 
+  static async pegarTodasLinhas(req, res) {
+    try {
+      const todasLinhas = await database.linhas.findAll();
+      res.status(200).json(todasLinhas);
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao obter todas as linhas.", details: error.message });
+    }
+  }
+
+  static async atualizarLinha(req, res) {
+    const { id } = req.params;
+    const novosDados = req.body;
+
+    try {
+      const linhaExistente = await database.linhas.findByPk(id);
+
+      if (!linhaExistente) {
+        return res.status(404).json({ error: "Linha não encontrada." });
+      }
+
+      await linhaExistente.update(novosDados);
+      return res.status(200).json({ message: `A linha: ${linhaExistente.nomeDaLinha} foi atualizada.` });
+    } catch (error) {
+      return res.status(500).json({ error: "Erro ao atualizar a linha.", details: error.message });
+    }
+  }
+
   static async deletarLinha(req, res) {
     const { id } = req.params; // Assume que o ID é passado como um parâmetro na URL
 
