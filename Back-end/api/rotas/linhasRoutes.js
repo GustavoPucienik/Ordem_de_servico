b9b545b9fefbab80948bdb664691c70e08d84c4a
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-11-13 00:43:29 -0300

    Adicionando os métodos as rotas

--- Back-end/api/rotas/linhasRoutes.js
+++ Back-end/api/rotas/linhasRoutes.js
@@ -5,7 +5,9 @@ const LinhasController = require("../controllers/linhasController.js");
 const router = express.Router();
 
 router
+  .get("/linha", LinhasController.pegarTodasLinhas)
   .post("/linha", LinhasController.criaLinha)
+  .put("/linha/:id", LinhasController.atualizarLinha)
   .delete("/linha/:id", LinhasController.deletarLinha);
 
 module.exports = router;
