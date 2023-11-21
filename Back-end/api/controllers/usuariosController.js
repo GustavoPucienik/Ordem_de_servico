487db9b4447a411e6a631eb34f8d8907dedbf935
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-11-21 19:44:42 -0300

    Implementando bcrypt e melhorando login e cadastrar

--- Back-end/api/controllers/usuariosController.js
+++ Back-end/api/controllers/usuariosController.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-bitwise */
 /* eslint-disable class-methods-use-this */
 const jwt = require("jsonwebtoken");
+const { hash, compare } = require("bcryptjs");
 const database = require("../models");
 
 const jwtSecret = process.env.JWT_SECRET;
@@ -12,14 +13,24 @@ class UsuarioController {
       const usuarioquelogou = await database.usuarios.findOne({
         where: {
           email,
-          senha,
         },
       });
       if (!usuarioquelogou || usuarioquelogou === null || usuarioquelogou === undefined) {
-        return res.status(200).json({ message: "Usuario não encontrado ou a senha ou email estão incorretos." });
+        return res.status(200).json({ msg: `Usuario com email ${email} não foi encontrado` });
       }
-      const token = jwt.sign({ email: usuarioquelogou.email, senha: usuarioquelogou.senha, id: usuarioquelogou.id }, jwtSecret, { expiresIn: "1h" });
-      return res.status(200).json({ usuario: usuarioquelogou, token });
+      const senhasIguais = await compare(senha, usuarioquelogou.senha);
+
+      if (!senhasIguais) {
+        return res.status(200).json({ msg: "A senha está incorreta!" });
+      }
+
+      const token = jwt.sign({
+        id: usuarioquelogou.id,
+        nome: usuarioquelogou.nome,
+        setor: usuarioquelogou.setor,
+        email: usuarioquelogou.email,
+      }, jwtSecret, { expiresIn: "2h" });
+      return res.status(200).json({ token });
     } catch (error) {
       return res.status(500).json(error.message);
     }
@@ -27,19 +38,28 @@ class UsuarioController {
 
   static async cadastrar(req, res) {
     const novoUsuario = req.body;
-    const userExiste = await database.usuarios.findOne({
-      where: {
-        email: novoUsuario.email,
-      },
-    });
-    if (userExiste) {
-      return res.status(200).json({ msg: "O email já está sendo utilizado." });
-    }
+
     try {
-      const novoUsuarioCriado = await database.usuarios.create(novoUsuario);
-      return res.status(201).json(novoUsuarioCriado);
+      const userExiste = await database.usuarios.findOne({
+        where: {
+          email: novoUsuario.email,
+        },
+      });
+      if (userExiste) {
+        return res.status(200).json({ msg: "O email já está sendo utilizado." });
+      }
+
+      const senhaHash = await hash(novoUsuario.senha, 8);
+
+      const novoUsuarioCriado = await database.usuarios.create({
+        nome: novoUsuario.nome,
+        setor: novoUsuario.setor,
+        email: novoUsuario.email,
+        senha: senhaHash,
+      });
+      return res.status(201).json({ nome: novoUsuarioCriado.nome });
     } catch (error) {
-      return res.status(500).json(error.message);
+      return res.status(500).json({ error: error.message });
     }
   }
 
@@ -54,7 +74,6 @@ class UsuarioController {
         const dadosUsuario = await database.usuarios.findOne({
           where: {
             email: decoded.email,
-            senha: decoded.senha,
           },
         });
         return res.status(200).json(dadosUsuario);
@@ -85,7 +104,7 @@ class UsuarioController {
         email,
       },
     });
-    res.status(200).json(dadosUsuario);
+    res.status(204).json(dadosUsuario);
   }
 
   static async deletarUsuario(req, res) {
